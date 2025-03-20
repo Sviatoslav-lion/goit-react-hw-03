d403fdc8e4be9832bfc90df263d10f194b255d4c
Sviatoslav <sviat.danko@gmail.com>
2025-03-20 14:48:01 +0000

    step two

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,5 +1,21 @@
-import css from './SearchBox.module.css';
 
-export default function SearchBoxFunc() {
-  return <></>;
-}
+import css from './SearchBox.module.css';
+import { useState } from 'react';
+
+const SearchBoxFunc = () => {
+  const [inputValue, setInputValue] = useState('');
+  const handleChange = evt => {
+    setInputValue(evt.target.value);
+  };
+
+  return (
+    <div>
+      <h2 className="visually-hidden">Find contact</h2>
+      <label>Finde contact by name</label>
+      <input type="text" value={inputValue} onChange={handleChange} />
+      <p>{inputValue}</p>
+    </div>
+  );
+};
+
+export default SearchBoxFunc;
